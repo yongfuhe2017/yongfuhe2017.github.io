8a5e0386737ee752913bb8c130e66151db360320
yongfuhe2017 <yongfuhe2017@163.com>
2017-06-23 23:09:49 +0800

    nwe

--- README.md
+++
@@ -1,5 +0,0 @@
-# yongfuhe2017.github.io
-Yongfu He's Personal Blog: https://yongfuhe2017.github.io/
-
-Yongfu He's Personal website: https://xriver007.github.io/
-
--- js/post.js
+++ js/post.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+﻿$(document).ready(function(){
 
     $('pre').addClass('prettyprint linenums'); // Google code Hight need these class
     $.getScript('/js/prettify/prettify.js', function(){ prettyPrint(); });
@@ -109,7 +109,7 @@ $(document).ready(function(){
     (function lastModifedTime() {
       var $lm = $('span.lastModified');
       $.ajax({
-        url: 'https://api.github.com/repos/litaotao/litaotao.github.io/commits?path=/' + $lm.data('source'),
+        url: 'https://api.github.com/repos/yongfuhe2017/yongfuhe2017.github.io/commits?path=/' + $lm.data('source'),
         method: 'GET',
         contentType: 'application/json',
         success: function(data) {
